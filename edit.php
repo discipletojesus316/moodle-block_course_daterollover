c70d6b7da822dc3a1aa9faa5c9e200da83f91c57
Ts Te <snort.test123@gmail.com>
2014-06-05 00:14:26 +0200

    Edit

--- edit.php
+++ edit.php
@@ -1,4 +1,31 @@
 <?php
+       // This file is part of Moodle - http://moodle.org/
+    //
+    // Moodle is free software: you can redistribute it and/or modify
+    // it under the terms of the GNU General Public License as published by
+    // the Free Software Foundation, either version 3 of the License, or
+    // (at your option) any later version.
+    //
+    // Moodle is distributed in the hope that it will be useful,
+    // but WITHOUT ANY WARRANTY; without even the implied warranty of
+    // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    // GNU General Public License for more details.
+    //
+    // You should have received a copy of the GNU General Public License
+    // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+    /**
+     *  block_course_daterollover
+     *
+     *  This plugin adjust assignment date according to the course start date , it will set forward assignment items  by x number of days, which includes allowsubmissionfrom,
+     *  due-date, cutoffdate, upcoming events, in a course through one centralized screen  
+     *  rather than having to go into each individual assignment activity.
+     *
+     * @author      Tsedey Terefe <snort.test123@gmail.com>
+     * @license     GNU General Public License version 3
+     * @package     block
+     * @subpackage  course_daterollover
+     */
 global $CFG,$DB,$course,$PAGE;
 require_once('../../config.php');
 
@@ -37,9 +64,6 @@ $form = new course_daterollover_form(null, array('coursecontext' => $courseconte
 
 
 if ($data = $form->get_data()) {
-
-
-$sql2=$DB->get_fieldset_select('course','startdate','id=?',array($courseid));
 
 
 
@@ -91,7 +115,6 @@ $courserecord->id = $a->id;
 $courserecord->startdate = $data->date;
 $DB->update_record('course', $courserecord);
 }
-
 
 
 
